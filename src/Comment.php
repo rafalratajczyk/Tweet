84d91dda3453d4d5a47e3dfb80519cd61204086b
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-10 20:06:20 +0200

    comment code

--- src/Comment.php
+++ src/Comment.php
@@ -6,8 +6,49 @@ class Comment
     private $id;
     private $userId;
     private $tweetId;
-    private $date;
+    private $creationDate;
     private $text;
+
+    static public function setConn(mysqli $newComment)
+    {
+        self::$conn = $newComment;
+    }
+
+    static public function newComments($userId, $tweetId, $text)
+    {
+        $sqlQuerry = "INSERT INTO Comments (user_id, tweet_id, creation_date, text )
+                VALUES ('$userId', '$tweetId', NOW(), '$text' )";
+        $result = self::$conn->query($sqlQuerry);
+
+        if ($result = true && strlen($result) <= 60) {
+            $myCommnents = new Comment(self::$conn->insert_id, $userId, $tweetId, $text, date("Y-m-d H:i:s"));
+
+            return $myCommnents;
+        }
+    }
+
+    static public function getCommentsByIdOfTweet($tweedId)
+    {
+        $sqlQuerry = "INSERT INTO Comments (user_id, tweet_id, creation_date, text )
+                VALUES ('$userId', '$tweetId', NOW(), '$text' )";
+        $result = self::$conn->query($sqlQuerry);
+        $array = [];
+
+        if ($result === true) {
+            while ($row = $result->fetch_assoc()) {
+                $commentsByIdOfTweet = new Comment(
+                    $row['comment_id'],
+                    $row['user_id'],
+                    $row['tweet_id'],
+                    $row['creation_date'],
+                    $row['text']
+                );
+                $array = $commentsByIdOfTweet;
+            }
+        }
+
+        return $array;
+    }
 
     /**
      * Comment constructor.
@@ -17,12 +58,12 @@ class Comment
      * @param $date
      * @param $text
      */
-    public function __construct($id, $userId, $tweetId, $date, $text)
+    public function __construct($id, $userId, $tweetId, $creationDate, $text)
     {
         $this->id = $id;
         $this->userId = $userId;
         $this->tweetId = $tweetId;
-        $this->date = $date;
+        $this->creationDate = $creationDate;
         $this->text = $text;
     }
 
@@ -45,9 +86,9 @@ class Comment
     /**
      * @return mixed
      */
-    public function getDate()
+    public function getCreationDate()
     {
-        return $this->date;
+        return $this->creationDate;
     }
 
     /**
@@ -65,7 +106,6 @@ class Comment
     {
         $this->text = $text;
     }
-
 
 
 }
