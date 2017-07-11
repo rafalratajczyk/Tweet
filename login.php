161dd2afbea897b97bac095e9a92d0cc53db3e3c
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-11 19:18:04 +0200

    code for login

---
+++ login.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once 'src/Connection.php';
+
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $user = Users::login($_POST["mail"], $_POST["password"]);
+
+    if ($user != false) {
+        $_SESSION["user"] = $user;
+        header("location: main.php");
+    }
+
+    echo "Bad password or login";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tweeter</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+            crossorigin="anonymous"></script>
+</head>
+<body>
+<h1>Welcome on Tweeter!</h1>
+
+<form action="login.php" method="POST">
+    <input type="text" name="mail" placeholder="Enter email">
+    <input type="password" name="password" placeholder="Enter password">
+    <input type="submit" value="login">
+</form>
+<div class="alert-warning">
+    <?php
+    ?>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
